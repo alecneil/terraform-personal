c170a5774c29cff0ad451b26068496ab1d1bc94e
Alec Neil Collado <alecneilcollado.w@gmail.com>
2023-04-02 13:17:39 +0800

    fixed the indentations

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 //disable this if the kms and bucket is not yet created
 terraform {
- backend "s3" {
-   bucket         = "my-tfstate-bucket0123"
-   key            = "state/terraform.tfstate"
-   region         = "us-east-1"
-   encrypt        = true
-   kms_key_id     = "alias/terraform-bucket-key"
-   dynamodb_table = "terraform-state"
- }
+  backend "s3" {
+    bucket         = "my-tfstate-bucket0123"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    kms_key_id     = "alias/terraform-bucket-key"
+    dynamodb_table = "terraform-state"
+  }
 }
